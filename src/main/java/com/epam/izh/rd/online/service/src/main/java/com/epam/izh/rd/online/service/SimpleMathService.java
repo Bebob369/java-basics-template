124e349f1fe0d618bfcdfa44472e5ad21c63e817
Bebob369 <60324042+Bebob369@users.noreply.github.com>
2021-05-19 20:52:46 +0300

    added solution

---
+++ src/main/java/com/epam/izh/rd/online/service/src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -0,0 +1,122 @@
+package com.epam.izh.rd.online.service;
+
+import java.util.Arrays;
+
+public class SimpleMathService implements MathService {
+
+    /**
+     * Метод возвращает 0, если value1 = value2.
+     * Метод возвращает -1, если value1 < value2.
+     * Метод возвращает 1, если value1 > value2.
+     *
+     * Например для (-1, -1) метод должен вернуть 0;
+     * Например для (-3, -1) метод должен вернуть -1;
+     * Например для (3, 1) метод должен вернуть 1;
+     */
+    @Override
+    public int compare(int value1, int value2) {
+        //return compare(value1, value2);
+        return (value1 < value2) ? -1 : ((value1 == value2) ? 0 : 1);
+    }
+
+
+    /**
+     * Метод возвращает максимальное число из пары.
+     * Например для списка (-1, 2) метод должен вернуть 2
+     */
+    @Override
+    public int maxFrom(int value1, int value2) {
+        return  (value1 > value2)? value1:value2;
+    }
+
+
+    /**
+     * Метод возвращает максимальное число из переданного массива.
+     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 22
+     */
+    @Override
+    public int maxFrom(int[] values){
+        return Arrays.stream(values).max().getAsInt();
+    }
+
+    /**
+     * Метод возвращает сумму чисел массива.
+     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 30
+     */
+    @Override
+    public int sum(int[] values) {
+        return  Arrays.stream(values).sum();
+    }
+
+    /**
+     * Метод фильтрует массив, оставляя только четные числа.
+     * Например для списка {-1, -3, 4, 8, 5, 22, 17} метод должен вернуть {4, 8, 22}
+     */
+    @Override
+    public int[] getEvenDigits(int[] values) {
+        return  Arrays.stream(values).filter(x -> x%2 == 0).toArray();
+    }
+
+    /**
+     * Метод считает факториал из заданного числа.
+     * Например для числа 5 метод должен вернуть 120.
+     * Факториал 0 должен быть равен 1.
+     */
+    @Override
+    public long calcFactorial(int initialVal) {
+        return (initialVal==0)?1:initialVal*calcFactorial(initialVal-1);
+    }
+
+    /**
+     * Метод возвращает число, которе находится на заданной позиции (счет начинается с нуля) в ряду фибоначчи.
+     *
+     * Ряд фибоначчи - ряд, следующие элементы которого состоят из суммы двух предыдущих.
+     * Ряд начинается 0 и 1.
+     * Пример 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
+     *
+     * Для числа 9 метод должен вернуть 34
+     * Для числа 0 метод должен вернуть 0
+     */
+    @Override
+    public long calcFibonacci(int number) {
+        return (number <= 1)?number:calcFibonacci(number-1)+calcFibonacci(number-2);
+    }
+
+    /**
+     * Метод возвращает отсортированный по возрастанию массив.
+     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть {-5, -3, -1, 4, 5, 8, 22}
+     */
+    @Override
+    public int[] sort(int[] values) {
+        return Arrays.stream(values).sorted().toArray();
+    }
+
+    /**
+     * Метод определяет, является ли заданное число простым.
+     * Простое число - число, которое делится только на 1 и на само себя.
+     *
+     * Например для числа 22 вернется false, а для числа 23 true.
+     */
+    @Override
+    public boolean isPrimary(int number) {
+        double s = Math.sqrt(number);
+        for(int i = 2; i <= s; i++)
+            if(number % i == 0) return false;
+        return number > 1;
+    }
+
+    /**
+     * Метод возвращает массив, в котором элементы расположены в обратном порядке.
+     *
+     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод вернет {-5, 22, 5, 8, 4, -3, -1}
+     */
+    @Override
+    public int[] reverseArray(int[] values) {
+        for (int i = 0; i < values.length / 2; i++) {
+            int temp = values[i];
+            values[i] = values[values.length - i - 1];
+            values[values.length - i - 1] = temp;
+        }
+        return values;
+    }
+}
